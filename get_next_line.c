55cc5e0b0c7a1c4e52f9c254ebe6c1c08099b8b3
tonTed <yddetco@gmail.com>
2021-09-29 23:38:18 -0400

    in good progress

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,13 @@
 /*   By: tblanco <tblanco@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/26 01:34:54 by tblanco           #+#    #+#             */
-/*   Updated: 2021/09/27 22:47:30 by tblanco          ###   ########.fr       */
+/*   Updated: 2021/09/29 23:36:08 by tblanco          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-#define BUFFER_SIZE 5
+#define BUFFER_SIZE 10
 
 size_t	ft_strlen(const char *s)
 {
@@ -22,25 +22,6 @@ size_t	ft_strlen(const char *s)
 	while (s[i])
 		i++;
 	return (i);
-}
-
-char	*ft_strjoin(char const *s1, char const *s2)
-{
-	char	*ret;
-	int		len;
-	int		i;
-
-	len = ft_strlen((char *)s1) + ft_strlen((char *)s2) + 1;
-	ret = malloc(len * sizeof(char));
-	if (!ret)
-		return (NULL);
-	i = 0;
-	while (i < len && *s1)
-		ret[i++] = *s1++;
-	while (i < len && *s2)
-		ret[i++] = *s2++;
-	ret[i] = 0;
-	return (ret);
 }
 
 char	*ft_strnew(size_t size)
@@ -58,29 +39,65 @@ char	*ft_strnew(size_t size)
 	return (s);
 }
 
+char	*ft_strjoin(char *ret, char  **buf, int len_buf)
+{
+	char	*ret_;
+	int		i;
+
+	ret_ = (char *)malloc((ft_strlen(ret) + len_buf + 1) * sizeof(char));
+	if (!ret_)
+		return (NULL);
+	i = 0;
+	while (*ret)
+		ret_[i++] = *ret++;
+	while (**buf && **buf != '\n')
+		ret_[i++] = *(*buf)++;
+	ret_[i++] = *(*buf)++;
+	ret_[i] = '\0';
+	return (ret_);
+}
+
+char	*ft_strjoin_(char *ret, char *buf, int *continue_, char **save)
+{
+	int		len;
+	char	*tmp_save;
+
+	len = 0;
+	while (buf[len] && buf[len] != '\n')
+		len++;
+	if (buf[len] == '\n')
+	{
+		*save = (char *)malloc(sizeof(char) * (BUFFER_SIZE - len));
+		if (!save)
+			return (NULL);
+		ret = ft_strjoin(ret, &buf, len + 1);
+		*continue_ = 0;
+		printf(" >> %s\n", buf);
+	}
+	return (ret);
+}
+
+// [0][1][2][3][4][5][6][7][8][9]
+//  T  E  D  D  Y  /n B  L  A  N
+
+//  10 - 5 = 5
+
 char	*ft_get_next_line(int fd)
 {
-	char		*buf;
+	char		buf[BUFFER_SIZE + 1];
 	int			char_read;
-	static char	*ret;
+	static char	*save;
+	char		*ret;
+	int			continue_;
 
-	int i = 0;
-	buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE +1));
-	if (!buf)
-		return (NULL);
-	
-	while ((char_read = read(fd, buf, BUFFER_SIZE)))
+	continue_ = 1;
+	ret = "";
+	while (continue_)
 	{
+		puts("########");
+		char_read = read(fd, buf, BUFFER_SIZE);
 		buf[char_read] = '\0';
-		printf("LOOP %d - char_read: %d - %s\n", i, char_read, buf);
-		if (!ret && char_read)
-			ret = ft_strjoin(ft_strnew(1), buf);
-		else
-		{
-			free(ret);
-			ret = ft_strjoin(ret, buf);
-		}
-		i++;
+		ret = ft_strjoin_(ret, buf, &continue_, &save);
 	}
 	printf("%s\n", ret);
 	return (ret);
---
+++ toDelete/main.c
@@ -0,0 +1,18 @@
+#include "../get_next_line.h"
+
+#include <fcntl.h>
+
+int main()
+{
+	int fd1 = open("toDelete/42", O_RDONLY);
+
+	if (fd1 == -1)
+		{puts("open() error"); return (1);}
+
+	ft_get_next_line(fd1);
+
+	if (close(fd1) == -1)
+		{puts("close() error"); return (1);}
+
+	return (0);
+}
