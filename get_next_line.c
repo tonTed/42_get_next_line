5f97eff469f56f5c120be69475ae6282d64a0ca5
Teddy Blanco <tblanco@c2r2p12.42quebec.com>
2021-10-02 18:47:02 -0400

    review whith mielpops

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,13 @@
 /*   By: tblanco <tblanco@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/26 01:34:54 by tblanco           #+#    #+#             */
-/*   Updated: 2021/10/02 10:31:20 by tblanco          ###   ########.fr       */
+/*   Updated: 2021/10/02 17:44:14 by tblanco          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-// #define BUFFER_SIZE 42
+#define BUFFER_SIZE 42
 
 char	*ft_strjoin(char *ret, char **buf, int len_buf)
 {
@@ -39,18 +39,20 @@ char	*ft_prejoin(char *ret, char *buf, int *continue_, char **save)
 	// char	*new_ret;
 
 	len = 0;
-	while (buf[len] && buf[len] != '\n')
+	while (buf[len] != '\0' && buf[len] != '\n')
 		len++;
 	ret = ft_strjoin(ret, &buf, len + 1);
-	if (*(buf - 1) == '\n')
+	if (*buf == '\0' || *(buf - 1) == '\n')
 	{
 		*continue_ = 0;	
-		if (*buf)
-		{
+		// if (*buf)
+		// {
 			tmp = *save;
+			// printf("BEFORE strdup > save: %s - buf: %s\n", *save, buf);
 			*save = ft_strdup(buf);
-			free(tmp);
-		}
+			// printf("AFTER strdup > save: %s\n", *save);
+			// free(tmp);
+		// }
 	}
 	return (ret);
 }
@@ -66,24 +68,27 @@ char	*get_next_line(int fd)
 
 	continue_ = 1;
 	char_read = 1;
-	ret = ft_strdup("");
-	tmp = ret;
 	ret = ft_prejoin("", save, &continue_, &save);
-	free(tmp);
 	while (continue_ && char_read)
 	{
 		char_read = read(fd, buf, BUFFER_SIZE);
+		buf[char_read] = '\0';
 		if (char_read == - 1 || (char_read == 0 && ret[0] == '\0'))
 		{
 			free(ret);
 			return (NULL);
 		}
-		// printf("char_read: %d, buffer: %s\n",char_read, buf);
-		buf[char_read] = '\0';
-		tmp = ret;
-		ret = ft_prejoin(ret, buf, &continue_, &save);
-		free(tmp);
+		if (char_read)
+		{
+			tmp = ret;
+			ret = ft_prejoin(ret, buf, &continue_, &save);
+			free(tmp);
+		}
+		else
+		{
+			save = "";
+			return (ret);
+		}
 	}
-	// printf("ret: %s - save: %s", ret, save);
 	return (ret);
 }
--- toDelete/main.c
+++ toDelete/main.c
@@ -2,18 +2,29 @@
 
 #include <fcntl.h>
 
-int main()
+int main(int argc, char **argv)
 {
-	int fd1 = open("toDelete/42", O_RDONLY);
+	if (argc != 2)
+		return 0;
+	int fd1 = open(argv[1], O_RDONLY);
 
-	if (fd1 == -1)
-		{puts("open() error"); return (1);}
 	
-	get_next_line(fd1);
-	// get_next_line(fd1);
+	if (fd1 == -1)
+	{
+		puts("open() error");
+		return (1);
+	}
+	
+	char	*s;
+	while ((s = get_next_line(fd1)))
+		printf("%s\n", s);
 
+	
 	if (close(fd1) == -1)
-		{puts("close() error"); return (1);}
+	{
+		puts("close() error"); 
+		return (1);
+	}
 
 	return (0);
 }
