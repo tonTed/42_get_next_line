c64295d7595d87e3b0fb956880c1b0283372ce08
Teddy Blanco <tblanco@c2r2p12.42quebec.com>
2021-10-04 09:51:52 -0400

    returns good end fd

--- get_next_line.c
+++ get_next_line.c
@@ -6,15 +6,25 @@
 /*   By: tblanco <tblanco@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/26 01:34:54 by tblanco           #+#    #+#             */
-/*   Updated: 2021/10/04 09:30:41 by tblanco          ###   ########.fr       */
+/*   Updated: 2021/10/04 09:44:27 by tblanco          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <unistd.h>
 
 #define BUFFER_SIZE 1
 
 char	*get_next_line(int fd)
 {
+	char		buffer[BUFFER_SIZE + 1];
+	char		*ret;
+	int			char_read;
 
+	char_read = read(fd, buffer, BUFFER_SIZE);
+	buffer[char_read] = '\0';
+	ret = ft_strdup(buffer);
+	if (*ret == '\0')
+		return (NULL);
+	return (ret);
 }
--- toDelete/main.c
+++ toDelete/main.c
@@ -2,29 +2,28 @@
 
 #include <fcntl.h>
 
-int main(int argc, char **argv)
+int main()
 {
-	if (argc != 2)
-		return 0;
-	int fd1 = open(argv[1], O_RDONLY);
+	// if (argc != 2)
+	// 	return 0;
+	// int fd1 = open(argv[1], O_RDONLY);
+
+	int fd1 = open("toDelete/42", O_RDONLY);
 
 	
 	if (fd1 == -1)
-	{
-		puts("open() error");
-		return (1);
-	}
+		{puts("open() error");return (1);}
 	
 	char	*s;
 	while ((s = get_next_line(fd1)))
-		printf("%s\n", s);
+		{
+			printf("%s\n", s);
+			// sleep(1);
+		}
 
 	
 	if (close(fd1) == -1)
-	{
-		puts("close() error"); 
-		return (1);
-	}
+		{puts("close() error");return (1);}
 
 	return (0);
 }
