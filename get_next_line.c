65af061cf6e8ae45af1a3784d70eac17818297a9
Teddy Blanco <tblanco@c2r2p12.42quebec.com>
2021-10-19 11:38:47 -0400

    v4 in progress

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,47 @@
 /*   By: tblanco <tblanco@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/26 01:34:54 by tblanco           #+#    #+#             */
-/*   Updated: 2021/10/19 09:07:45 by tblanco          ###   ########.fr       */
+/*   Updated: 2021/10/19 10:57:35 by tblanco          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+#define BUFFER_SIZE 1
+#include <stdio.h>
+
 char	*get_next_line(int fd)
 {
+	int		char_read;
+	char	buffer[BUFFER_SIZE + 1];
+	char	*ret;
+	// char	*tmp;
 
+	// if (fd < 0)
+	// 	return (NULL);
+	ret = NULL;
+	// printf ("fd: %d\n", fd);
+	char_read = BUFFER_SIZE;
+	while (char_read == BUFFER_SIZE)
+	{
+		char_read = read(fd, buffer, BUFFER_SIZE);
+		// printf ("char_read: %d\n", char_read);
+		if (char_read == -1)
+			return (NULL);
+		buffer[char_read] = '\0';
+		if (ret)
+			ret = ft_strjoin(ret, buffer);
+		else
+			ret = ft_strdup(buffer);
+	}
+	// if (!ret)
+	// 	ret = ft_strdup(buffer);
+	// else
+	// {
+	// 	tmp = ret;
+	// 	ret = ft_strjoin(ret, buffer);
+	// 	free(tmp);
+	// } 
+	
+	return (ret);
 }
---
+++ test_gnl/main.c
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tblanco <tblanco@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/19 09:08:55 by tblanco           #+#    #+#             */
+/*   Updated: 2021/10/19 10:51:09 by tblanco          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../get_next_line.h"
+#include <stdio.h>
+#include <fcntl.h>
+#include <string.h>
+
+#define LEN_TAB 50
+
+/*
+	fd < 0 : error
+	fd = 0 : stdin 
+	fd = 1 : stdout 
+	fd = 2 : stderr
+	fd > 2 : files open 
+*/
+
+int	ft_strcmp(const char *s1, const char *s2)
+{
+	while (*s1 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return (*(unsigned char *) s1 - *(unsigned char *) s2);
+}
+
+int main(void)
+{
+	int     fd[LEN_TAB];
+	char    *s;
+	int 	errors = 0;
+	int		i;
+	char	*ret;
+
+	i = 0;
+
+	/* 0 */
+	fd[i] = -1;
+	if ((s = get_next_line(fd[i])) != NULL)
+/* 0 */	{printf("-- [%d]: fd(%d) negative must be return NULL >> %s\n",			i, fd[i], s);	errors++;}	i++;
+	fd[i] = 5000;
+	if ((s = get_next_line(fd[i])) != NULL)
+/* 1 */	{printf("-- [%d]: fd(%d) doesn't exist must be return NULL >> %s\n",	i, fd[i], s);	errors++;}	i++;
+	fd[i] = open("file", O_RDONLY); close(fd[i]);
+	if ((s = get_next_line(fd[i])) != NULL)
+/* 2 */	{printf("-- [%d]: fd(%d) close() must be return NULL >> %s\n", 			i, fd[i], s);	errors++;}	i++;
+	fd[i] = open("empty", O_RDONLY);
+	if ((s = get_next_line(fd[i])) != NULL)
+/* 3 */	{printf("-- [%d]: fd(%d) file empty must be return NULL >> %s\n",		i, fd[i], s);	errors++;}	i++;
+	fd[i] = open("10_no_nl", O_RDONLY);
+	ret = "0123456789";
+	s = get_next_line(fd[i]);
+	if (s == NULL || strcmp(ret, s) != 0)
+/* 4 */	{printf("-- [%d]: fd(%d) must be return %s >> %s\n",					i, fd[i], ret, s);	errors++;}	i++;
+
+
+
+	printf("\nTOTAL ERRORS : %d/%d\n", errors, i);
+	return 0;
+}
