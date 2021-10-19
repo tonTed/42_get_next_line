5a6085367598eb7649c3cdaa80bc20c29029d5eb
tonTed <yddetco@gmail.com>
2021-10-19 08:14:24 -0400

    go to new version :,(

--- get_next_line.c
+++ get_next_line.c
@@ -6,90 +6,93 @@
 /*   By: tblanco <tblanco@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/26 01:34:54 by tblanco           #+#    #+#             */
-/*   Updated: 2021/10/18 16:18:15 by tblanco          ###   ########.fr       */
+/*   Updated: 2021/10/19 08:09:50 by tblanco          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*ft_realloc(char *tofree, char *newchar)
-{
-	free(tofree);
-	return (newchar);
-}
+// #define BUFFER_SIZE 3
+// #include <stdio.h>
 
-void	*ft_freenull(char **ptr)
-{
-	free(*ptr);
-	return (NULL);
-}
-
-char	*ft_strcut(char *s, char c, char **save)
+char	*ft_freedup(char **s)
 {
 	char	*ret;
-	int		len;
-	int		i;
 
-	i = 0;
-	len = 0;
-	while (s[i++] != c)
-		len++;
-	len++;
-	ret = (char *)malloc(((len + 1) * sizeof(char)));
-	if (!ret)
-		return (NULL);
-	i = 0;
-	while (len--)
-		ret[i++] = *s++;
-	ret[i] = '\0';
-	if (*save)
-		free(*save);
-	*save = ft_strdup(s);
+	ret = ft_strdup(*s);
+	free(*s);
 	return (ret);
 }
 
-char	*ft_setting(char **save, int *char_read)
+char	*ft_freejoin(char *s1, char *s2)
 {
 	char	*ret;
+	ret = ft_strjoin(s1, s2);
+	free(s1);
+	return (ret);
+}
 
-	*char_read = BUFFER_SIZE;
-	if (!*save)
-		ret = ft_strdup("");
-	else if (ft_charinstr(*save, '\n'))
-	{
-		char_read = 0;
-		ret = ft_strcut(*save, '\n', &*save);
-	}
-	else
-	{
-		ret = ft_strdup(*save);
-		free(*save);
-		*save = NULL;
-	}
+char	*ft_strcut(char *s, char sep, char **save)
+{
+	char	*tmp;
+	char	*ret;
+	int		i;
+
+	i = 0;
+	while (s[i] != sep)
+		i++;
+	ret = (char *)malloc(sizeof(char) * (i + 2));
+	if (!ret)
+		return (NULL);
+	i = 0;
+	while (*s != sep)
+		ret[i++] = *s++;
+	ret[i++] = *s++;
+	ret[i] = '\0';
+	tmp = *save;
+	if (*save)
+		*save = ft_strdup(s);
+	free(tmp);
 	return (ret);
 }
 
 char	*get_next_line(int fd)
 {
-	char		buffer[BUFFER_SIZE + 1];
 	char		*ret;
-	int			char_read;
 	static char	*save = NULL;
+	int			char_read;
+	char		buffer[BUFFER_SIZE + 1];
 
 	if (fd < 0)
 		return (NULL);
-	ret = ft_setting(&save, &char_read);
-	while (ft_charinstr(ret, '\n') == 0 && char_read == BUFFER_SIZE)
+	if (save && ft_charinstr(save, '\n'))
+		return (ft_strcut(save, '\n', &save));
+	ret = NULL;
+	if (save)
+		ret = ft_freedup(&save);
+	char_read = BUFFER_SIZE;
+	while (char_read == BUFFER_SIZE)
 	{
 		char_read = read(fd, buffer, BUFFER_SIZE);
-		if (char_read == -1 )
-			return (ft_freenull(&ret));
-		buffer[char_read] = '\0';
-		ret = ft_realloc(ret, ft_strjoin(ret, buffer));
-		if (ft_charinstr(ret, '\n') != 0)
-			ret = ft_realloc(ret, ft_strcut(ret, '\n', &save));
+		if (char_read == -1)
+			return (NULL);
+		buffer[(int)char_read] = '\0';
+		if (char_read && ft_charinstr(buffer, '\n'))
+		{
+			if (ret)
+				return (ft_freejoin(ret, ft_strcut(buffer, '\n', &save)));
+			else
+				return (ft_strcut(buffer, '\n', &save));
+		}
+		else if (*buffer)
+		{
+			if (ret)
+				ret = ft_freejoin(ret, ft_strcut(buffer, '\0', &save));
+			else
+				ret = ft_strcut(buffer, '\0', &save);
+		}
 	}
-	if (*ret == '\0')
-		return (ft_freenull(&ret));
-	return (ret);
+	if (ret)
+		return (ret);
+	return (NULL);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: tblanco <tblanco@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/04 20:04:05 by tblanco           #+#    #+#             */
-/*   Updated: 2021/10/05 10:02:19 by tblanco          ###   ########.fr       */
+/*   Updated: 2021/10/19 00:16:04 by tblanco          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,8 +26,8 @@ char	*ft_strjoin(char *s1, char *s2);
 
 /* get_next_line */
 char	*get_next_line(int fd);
-char	*ft_realloc(char *tofree, char *newchar);
-void	*ft_freenull(char **ptr);
-char	*ft_strcut(char *s, char c, char **save);
+char	*ft_strjoin_free(char *s1, char *s2);
+char	*ft_strsep(char **save, char *buffer, char sep, char *cur_ret);
+char	*ft_gettosep(char **s, char sep);
 
 #endif
