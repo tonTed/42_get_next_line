4f3d58e52536d0a5f9a69ea3ec84874122268b1d
tonTed <yddetco@gmail.com>
2021-10-25 19:38:56 -0400

    testing things with ptr

--- main.c
+++ main.c
@@ -1,17 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void    print_adress(char *s)
+void    print_adress(char *s, char **m)
 {
-    printf("Inside function: %p\n", s);
+    // printf("Inside function: %p\n", s);
+    printf("Inside function m: %p\n", m);
+    printf("Inside function *m: %p\n", *m);
+
+    *m = malloc(10);
+
+    printf("Inside function finish m: %p\n", m);
+    printf("Inside function finish *m: %p\n", *m);
 }
 
 int main(void)
 {
     char *s = malloc(10);
+    char *m;
     
-    printf("Before function: %p\n", s);
-    print_adress(s);
-    printf("After function: %p\n", s);
+    // printf("Before function s: %p\n", s);
+    printf("Before function &m: %p\n", &m);
+    printf("Before function m: %p\n", m);
+    print_adress(s, &m);
+    // printf("After function s: %p\n", s);
+    printf("After function &m: %p\n", &m);
+    printf("After function m: %p\n", m);
+
     return 0;
 }
